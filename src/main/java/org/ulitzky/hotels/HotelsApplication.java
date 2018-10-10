0c14706eaf93fb59abe4fffc317ae07440519c5b
Leonid Ulitsky <ulitsky@gmail.com>
2018-10-10 14:50:20 +0200

    Setup pom and spring-boot

---
+++ src/main/java/org/ulitzky/hotels/HotelsApplication.java
@@ -0,0 +1,16 @@
+package org.ulitzky.hotels;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lulitzky on 19.04.18.
+ */
+@SpringBootApplication
+public class HotelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsApplication.class, args);
+    }
+}
